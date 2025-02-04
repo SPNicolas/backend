bf70b06d6226e42ddddc453bce6b4168f77509ad
SPNicolas <nicolassteffenspereira@gmail.com>
2025-02-04 15:18:18 -0300

    fazendo

--- alunos/models.py
+++ alunos/models.py
@@ -6,3 +6,21 @@ class Estado(models.Model):
 
     def __str__(self):
         return f"{self.nome} ({self.sigla})"
+    
+class Cidade(models.Model):
+    nome = models.CharField(max_length=50)
+    estado = models.ForeignKey(Estado, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.nome
+    
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=12)
+    email = models.EmailField(max_length=100)
+    dataDeNascimento = models.DateField()
+    cidade =  models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.nome}, {self.telefone}, {self.email}, {self.dataDeNascimento}, {self.cidade}"
+
